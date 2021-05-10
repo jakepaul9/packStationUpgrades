aec48f5ac36cd6bbc0f47d2d554273f0037b028d
jakepaul9 <jacob.grether@gmail.com>
2021-05-10 15:49:41 -0600

    Update packingImprovements.js

--- packingImprovements.js
+++ packingImprovements.js
@@ -252,7 +252,7 @@ const aliases = [
 
 document.addEventListener('keydown', function(e) {
   let items = document.getElementsByClassName('view-item-name')
-  if (barcodeFlag && items.length > 0 && e.target.value.length > 1 && e.key == 'Enter') {
+  if (barcodeFlag && items.length > 0 && e.target.value && e.key == 'Enter') {
     if(e.target.value.includes('SCREEN_SCAN')){
     for (item of items) {
         if(barcodeCheck.includes(item.innerHTML.toUpperCase())){
